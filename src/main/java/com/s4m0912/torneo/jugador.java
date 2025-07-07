c959d08daf184316c7837f8f46bb88d53172676d
S4m0912 <saamuel.quintero@gmail.com>
2025-07-07 00:01:24 -0300

    agregue getters, setters y un atributo equipoJuega que indica a que equipo pertenece un jugador

--- src/main/java/com/s4m0912/torneo/jugador.java
+++ src/main/java/com/s4m0912/torneo/jugador.java
@@ -2,6 +2,8 @@ public class jugador extends persona {
 
     protected String posicion;
     protected int dorsal;
+    protected int cantidadGoles; 
+    protected equipo equipoJuega;
 
     public jugador(String nombre, String dni, int edad, String posicion, int dorsal){
 
@@ -9,6 +11,32 @@ public class jugador extends persona {
         if (dorsal < 0) { throw new IllegalArgumentException("El dorsal debe tener un numero mayor o igual a 0 ");}
         this.posicion = posicion;
         this.dorsal = dorsal;
+        this.cantidadGoles = 0;
+        equipoJuega = null;
+
+    }
+
+    public void setEquipoJuega(equipo equipo){
+
+        this.equipoJuega = equipo;
+
+    }
+
+    public equipo getEquipoJuega(){
+
+        return equipoJuega;
+
+    }
+
+    public int getCantidadGoles(){
+
+        return cantidadGoles;
+
+    }
+
+    public void anotarGol(){
+
+        cantidadGoles++;
 
     }
 
@@ -18,9 +46,9 @@ public class jugador extends persona {
 
     }
 
-    public void obtenerRol() {
+    public String obtenerRol(){
      
-                System.out.print(posicion);
+        return posicion;
 
     }
     
